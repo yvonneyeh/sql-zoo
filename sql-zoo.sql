1f7914b091618f23430c56af55cebcfc76169a1c
Yvonne Yeh <yvonneyeh@me.com>
2021-01-27 02:52:54 +0000

    Complete 13/13 exercises

--- sql-zoo.sql
+++ sql-zoo.sql
@@ -16,7 +16,9 @@ Andorra	Europe	468	78115	3712000000
 Angola	Africa	1246700	20609294	100990000000
 
 
-1
+-----
+
+1. Introduction
 
 -----
 
@@ -30,7 +32,7 @@ SELECT name, continent, population FROM world
 
 
 ==========
-2
+2. Large Countries
 
 -----
 
@@ -44,7 +46,7 @@ SELECT name FROM world WHERE population > 200000000
 
 
 ==========
-3
+3. Per Capita GDP
 
 -----
 
@@ -57,88 +59,175 @@ SELECT name, gdp/population FROM world WHERE population>200000000
 
 
 ==========
-4
+4. South America In Millions
+
+-----
+
+Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 1000000 to get population in millions.
 
 -----
 
 
-
------
-
-
-
+SELECT name, population/1000000 FROM world WHERE continent='South America'
 ;
 
 
 ==========
-5
+5. France, Germany, Italy
+
+-----
+
+Show the name and population for France, Germany, Italy
 
 -----
 
 
-
------
-
-
-
+SELECT name, population FROM world WHERE name IN ('France','Germany','Italy')
 ;
 
 
 ==========
-6
+6. United
+
+-----
+
+Show the countries which have a name that includes the word 'United'
 
 -----
 
 
-
------
-
-
-
+SELECT name FROM world WHERE name LIKE '%United%'
 ;
 
 
 ==========
-7
+7. Two Ways To Be Big
+
+-----
+
+Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+
+Show the countries that are big by area or big by population. Show name, population and area.
 
 -----
 
 
-
------
-
-
-
+SELECT name, population, area FROM world WHERE area>=3000000 OR population>=250000000
 ;
 
 
 ==========
-8
+8. One or the Other (but not both)
+
+-----
+
+Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+
+Australia has a big area but a small population, it should be included.
+Indonesia has a big population but a small area, it should be included.
+China has a big population and big area, it should be excluded.
+United Kingdom has a small population and a small area, it should be excluded.
 
 -----
 
 
-
------
-
-
-
+SELECT name, population, area FROM world WHERE area>=3000000 XOR population>=250000000
 ;
 
 
 ==========
-9
+9. Rounding
+
+-----
+
+Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+
+For South America show population in millions and GDP in billions both to 2 decimal places.
 
 -----
 
 
-
------
-
-
-
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world WHERE continent='South America'
 ;
 
 
 ==========
-10
+10. Trillion dollar economies
+
+-----
+
+Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+
+Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+
+-----
+
+
+SELECT name, ROUND(gdp/population, -3) FROM world WHERE gdp>=1000000000000
+;
+
+
+==========
+11. Name and Capital have the same length
+
+-----
+
+Greece has capital Athens.
+
+Each of the strings 'Greece', and 'Athens' has 6 characters.
+
+Show the name and capital where the name and the capital have the same number of characters.
+
+You can use the LENGTH function to find the number of characters in a string
+
+-----
+
+
+SELECT name, capital FROM world WHERE LENGTH(name)=LENGTH(capital)
+;
+
+
+==========
+12. Matching Name and Capital
+
+-----
+
+The capital of Sweden is Stockholm. Both words start with the letter "S".
+
+Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+You can use the function LEFT to isolate the first character.
+You can use <> as the NOT EQUALS operator.
+
+-----
+
+
+SELECT name, capital FROM world WHERE LEFT(name,1)=LEFT(capital,1) AND name<>capital
+;
+
+
+==========
+13. All the Vowels
+
+-----
+
+Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
+
+Find the country that has all the vowels and no spaces in its name.
+
+You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
+The query shown misses countries like Bahamas and Belarus because they contain at least one 'a'
+
+-----
+
+
+SELECT name FROM world
+WHERE name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
+AND name NOT LIKE '% %'
+;
+
+
+==========
